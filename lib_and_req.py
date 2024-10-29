79c960688b977c41fcb6f377bed5975e8329bc7f
BhuvanSharmAa <139460666+BhuvanSharmAa@users.noreply.github.com>
2024-10-29 09:46:44 +0530

    Update lib_and_req.py

--- lib_and_req.py
+++ lib_and_req.py
@@ -7,8 +7,13 @@ from transformers import pipeline
 from ultralytics import YOLO
 from multiprocessing import Process, Queue, Value
 import time
+import os
 
+if not os.path.exists("coco.names"):
+    import urllib.request
 
+    url = "https://raw.githubusercontent.com/pjreddie/darknet/master/data/coco.names"
+    urllib.request.urlretrieve(url, "coco.names")
 model = YOLO('yolov8n.pt')
 qa_pipeline = pipeline("question-answering", model="distilbert-base-cased-distilled-squad")
 engine = pyttsx3.init()
